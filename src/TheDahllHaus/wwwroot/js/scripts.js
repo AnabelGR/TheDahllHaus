e34400e53d75da12d81edeca37c443ac6623658f
Anabel <anabel@apparentinc.com>
2017-08-29 13:18:13 -0700

    change UI to MUI, refractor image files.

--- src/TheDahllHaus/wwwroot/js/scripts.js
+++ src/TheDahllHaus/wwwroot/js/scripts.js
@@ -1,6 +1,43 @@
-﻿jQuery(document).ready(function ($) {
-    //if ($('#menu1').hasClass('menu-full-width')) {
-    //$(this).addClass('hellooo');
-    jQuery('ul.uk-navbar-nav li .menu-full-width').parent().attr('data-uk-dropdown', "{justify: '#menu1'}");
-    //}
+﻿jQuery(function ($) {
+    var $bodyEl = $('body'),
+        $sidedrawerEl = $('#sidedrawer');
+
+
+    function showSidedrawer() {
+        // show overlay
+        var options = {
+            onclose: function () {
+                $sidedrawerEl
+                  .removeClass('active')
+                  .appendTo(document.body);
+            }
+        };
+
+        var $overlayEl = $(mui.overlay('on', options));
+
+        // show element
+        $sidedrawerEl.appendTo($overlayEl);
+        setTimeout(function () {
+            $sidedrawerEl.addClass('active');
+        }, 20);
+    }
+
+
+    function hideSidedrawer() {
+        $bodyEl.toggleClass('hide-sidedrawer');
+    }
+
+
+    $('.js-show-sidedrawer').on('click', showSidedrawer);
+    $('.js-hide-sidedrawer').on('click', hideSidedrawer);
+});
+
+var $titleEls = $('strong', $sidedrawerEl);
+
+$titleEls
+  .next()
+  .hide();
+
+$titleEls.on('click', function () {
+    $(this).next().slideToggle(200);
 });
